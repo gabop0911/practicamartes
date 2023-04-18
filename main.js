9f60115c8cc8af98c1725146e579a50d48cf2daa
GAbriel Prieto <a353297@uach.mx>
2023-04-18 22:02:40 +0000

    Se agrega menu.js

---
+++ main.js
@@ -0,0 +1,8 @@
+const Block = require('./Block');
+const Blockchain = require('./blockchain');
+
+
+const blockchain = new Blockchain("holassasasasaasa");
+blockchain.addBlock("segundo bloqueassada");
+
+console.log(blockchain);
